e29a9b1d52355df211548d00d5a4008f646b89dc
user <iOS-School-1@iMac-user-4.local>
2017-06-03 12:03:32 +0300

    Initial Commit

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by iOS-School-1 on 03.06.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimationTest
+//
+//  Created by iOS-School-1 on 03.06.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
